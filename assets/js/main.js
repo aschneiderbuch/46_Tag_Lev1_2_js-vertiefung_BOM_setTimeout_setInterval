71d373e1fa65c6f16e1e9c5ee4fa13772f0a2888
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 15:21:18 +0100

    setIntervalCounterStyleIf

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,11 +13,67 @@ querySelector
 
 */
 
+
+
+
 // btn
+const btnStart = document.querySelector("#btn");
+
+
 // click
-// function
-// counter 100
-// setInterval    
+btnStart.addEventListener("click", () => {
+    console.log("btn clicked");
+
+    // function def
+    function zählen() {
+        console.log("in function zählen");
+
+        // counter 100
+        let counter = 100;
+
+        // setInterval    
+        const stopInterval = setInterval(() => {
+            console.log("in function setInterval");
+
+            counter = counter - 1;
+            console.log(counter);
+
+            // / ausgabe counter
+            // text output
+            const textOutput = `${counter} %`;
+            console.log(textOutput);
+
+            // creat Element     --> !! entfällt, da ja div schon da zum einfügen siehe 100% Anfangswet
+           const neuerPTag = document.createElement("p");
+
+          // inhalt zuweisen
+              // / output 
+              const outputField = document.querySelector(`#output`);
+              console.log(outputField);
+              outputField.innerHTML = textOutput;
+              outputField.style.width = `${counter}px`;
+
+              // text im html anzeigen      --> !! entfällt, da ja div schon da
+           outputField.appendChild(neuerPTag);
+
+            // if    zum stoppen 
+            if (counter === 0) {
+                console.log("in  if");
+                console.log(counter)
+                clearInterval(stopInterval);
+            };
+
+
+        }, 100);
+
+    }
+
+    // function aufruf
+    zählen();
+
+});
+
+
 
 // im html div    - class zeit anzeigen   überschreiben   ersetzen
 
