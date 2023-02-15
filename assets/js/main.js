ff8c573004e1da057985d40bb757d8c7d04dde6a
aschneiderbuch <aschneiderbuch2@gmail.com>
2023-02-15 14:19:24 +0100

    html

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,25 @@
 console.log("test");
 
 /* 
+Function 
+    beim drücken des btn 
+        von 100 % bis 0 % runter zählt
 
-
+onclick
+setInterval
+clearInterval
+if, else if
+querySelector
 
 */
+
+// btn
+// click
+// function
+// counter 100
+// setInterval    
+
+// im html div    - class zeit anzeigen   überschreiben   ersetzen
+
+
+// evtl. noch, das sich der Balken mit der zeit verlängert :-)
